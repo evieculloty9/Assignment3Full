d550b243978e041c031cd987cf5afca2d6e6a11e
evieculloty <120426486@umail.ucc.ie>
2022-11-30 23:19:55 +0000

    blueThreeArgsConstructorZerosTest

--- test/java/ColourTest.java
+++ test/java/ColourTest.java
@@ -16,3 +16,9 @@ public class ColourTest {
         Colour col = new Colour(0.0, 0.0, 0.0);
         Assertions.assertEquals(0, col.getGreen());
     }
+
+    @Test
+    public void blueThreeArgsConstructorZerosTest() {
+        Colour col = new Colour(0.0, 0.0, 0.0);
+        Assertions.assertEquals(0.0, col.getBlue());
+    }
